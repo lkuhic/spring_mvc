b7a503f7b71cac2e31858039452581755515e4f0
lkuhic <evgenijborskov@laptop-836s21m7.sigma.sbrf.ru>
2020-09-09 18:27:09 +0300

    Chapter XV. JSON XML REST

---
+++ src/main/java/ru/javastudy/mvcHtml5Angular/mvc/rest/model/DBLogsXML.java
@@ -0,0 +1,19 @@
+package ru.javastudy.mvcHtml5Angular.mvc.rest.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "LOGS")
+public class DBLogsXML {
+    private List<DBLogXML> logList;
+
+    @XmlElement(name = "LOG")
+    public List<DBLogXML> getLogList() {
+        return logList;
+    }
+
+    public void setLogList(List<DBLogXML> logList) {
+        this.logList = logList;
+    }
+}
