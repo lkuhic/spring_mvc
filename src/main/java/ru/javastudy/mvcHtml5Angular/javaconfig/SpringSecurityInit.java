82a8f77a2cc79f9c11ae6aebce8f56903483ad71
lkuhic <evgenijborskov@cab-wsn-0005531.sigma.sbrf.ru>
2020-09-16 17:08:27 +0300

    java configs

---
+++ src/main/java/ru/javastudy/mvcHtml5Angular/javaconfig/SpringSecurityInit.java
@@ -0,0 +1,6 @@
+package ru.javastudy.mvcHtml5Angular.javaconfig;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInit extends AbstractSecurityWebApplicationInitializer{
+}
